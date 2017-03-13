8abac358b927c67ea3764bcbbd04d173344f0534
MrHertz <rus.syndicate@gmail.com>
2017-03-13 14:01:28 +0300

    fancybox edit

---
+++ Theme/send.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kilohertz
+ * Date: 13.03.17
+ * Time: 13:56
+ */
+$subject = "Заказ оформлен";
+$message =  'Имя: '.$_POST['name'].
+            '\nПочта: '.$_POST['email'].
+            '\nТелефон: '.$_POST['phone'].
+            '\nЗаказ: '.$_POST['message'];
+
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+mail('rus.syndicate@gmail.com', $subject, $message, $headers);
+?>
