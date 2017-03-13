b99de8a025b5cade669d320a33afc2d95fc57c8e
MrHertz <rus.syndicate@gmail.com>
2017-03-13 14:13:51 +0300

    fancybox edit

--- Theme/send.php
+++ Theme/send.php
@@ -5,13 +5,22 @@
  * Date: 13.03.17
  * Time: 13:56
  */
+$email = 'rus.syndicate@gmail.com';
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+if (isset($_POST['subscribe'])) {
+    mail($email, 'Подписка оформлена', $_POST['subscribe'], $headers);
+    Header('Location: /');
+    exit();
+}
+
 $subject = "Заказ оформлен";
 $message =  "Имя: ".$_POST['name'].
             "\nПочта: ".$_POST['email'].
             "\nТелефон: ".$_POST['phone'].
             "\nЗаказ: ".$_POST['message'];
 
-$headers= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
-mail('rus.syndicate@gmail.com', $subject, $message, $headers);
+mail($email, $subject, $message, $headers);
+Header('Location: /');
 ?>
