ef80beb255637da7829289b94747b4a690a9b711
MrHertz <rus.syndicate@gmail.com>
2017-03-17 17:48:07 +0300

    content

--- send.php
+++ send.php
@@ -5,7 +5,7 @@
  * Date: 13.03.17
  * Time: 13:56
  */
-$email = 'rus.syndicate@gmail.com';
+$email = 'support.magicwand@yandex.ru';
 $headers= "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
 
